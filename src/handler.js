e56b15c55b937d52ce13585fe1732be487a99f4f
Najmi Raihan <raihannajmi13@gmail.com>
2023-01-18 14:20:28 +0700

    fix payload

--- src/handler.js
+++ src/handler.js
@@ -2,8 +2,9 @@ const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
-  const { title, tags, body } = request.paylod;
+  const { title, tags, body } = request.payload;
 
+  console.log('title', title);
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
